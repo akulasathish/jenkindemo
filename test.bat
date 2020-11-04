904581a70d06c68c4bf7e0a528abd2fc2525285d
akulasathish <71421128+akulasathish@users.noreply.github.com>
2020-11-04 07:44:08 +0530

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,8 @@
+
+echo off
+echo "this is my first prgram wriiten "
+echo "*********************************************"
+echo " "
+systeminfo >>c:\jenkindemo\inventory.txt
+echo "    "
+echo " The Invenyory of the System is get collctect"
